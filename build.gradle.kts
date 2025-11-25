442f548de916a87e460dbd25e9db4424f5e9e4e7
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2025-11-25 12:16:13 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,15 @@ repositories {
 }
 
 dependencies {
-    implementation("javax.jms:jms-api:2.0.1")
-    implementation("org.apache.activemq:activemq-broker:6.1.1")
+    implementation("com.rabbitmq:amqp-client:5.21.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+
     testImplementation(kotlin("test"))
 }
 
@@ -21,7 +28,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(21)
 }
 
 application {
